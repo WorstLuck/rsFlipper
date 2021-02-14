86814565bc21be922b70b76d512733a6d516e9e6
Abdullah <43624157+WorstLuck@users.noreply.github.com>
2021-02-13 20:38:33 -0500

    no message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 import dash_auth
 
 
-USERNAME_PASSWORD_PAIRS = [['polarstar', 'carpet']]
+USERNAME_PASSWORD_PAIRS = [['karli', 'boi']]
 
 external_stylesheets =[dbc.themes.SIMPLEX]#, 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css']#,,dbc.themes.BOOTSTRAP]
 
